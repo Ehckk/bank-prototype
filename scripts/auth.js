25ad0bfe85d6242e434608250fdb1cedc89259ba
ehck <throwawayehcksdee@gmail.com>
2024-04-09 09:42:33 -0400

    logout function

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,13 +1,20 @@
-function check() {
+function check(isAuthenticated=true, redirect="/") {
     const user = window.localStorage.getItem("user")
-    if (!user) {
-        window.location.replace("/")
+    const valid = Boolean(user) === isAuthenticated
+    if (!valid) {
+        window.location.replace(redirect)
     }
 }
 
-function authenticate(user) {
+function login(user) {
     const data = JSON.stringify(user)
     window.localStorage.setItem("user", data)
+    window.location.replace("/home.html")
+}
+
+function logout() {
+    window.localStorage.removeItem("user")
+    window.location.replace("/")
 }
 
 function currentUser() {
@@ -21,6 +28,7 @@ function currentUser() {
 
 export default {
     check,
-    authenticate,
+    login,
+    logout,
     currentUser
 }
