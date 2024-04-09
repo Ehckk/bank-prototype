c0a9cd149dff4e4b44b15f8a00b44b1edb01eb9c
ehck <throwawayehcksdee@gmail.com>
2024-04-09 09:12:08 -0400

    fetchOne and fetchMany

--- scripts/data.js
+++ scripts/data.js
@@ -1,22 +1,52 @@
 const FILE_ROOT = `data`
 
+async function filterData(data, predicate) {
+    if (predicate) {
+        return data.filter(predicate)
+    }
+    return data
+}
+
+async function sortData(data, order) {
+    if (order) {
+        data.sort(order)
+    }
+    return data
+}
+
 async function fetchData(params) {
-    const {name, predicate, one} = params
+    const {name, predicate, order} = params
     if (!name) {
         throw new Error("Must specify file name to fetch from")
     } 
     const path = `${FILE_ROOT}/${name}.json` 
     const res = await fetch(path)
-    console.log(res)
-    const data = await res.json() 
+    const data = await res.json()
     console.log(data)
-    
-    if (!predicate) {
-        
-    }
+    const results = sortData(filterData(data, predicate), order)  
+    console.log(results)
     return data
 }
 
+async function fetchOne(params) {
+    const data = await fetchData(params)
+    if (data.length === 0) {
+        throw new Error("Not found")
+    }
+    return data[0]
+}
+
+async function fetchMany(params) {
+    const {limit} = params
+    const data = await fetchData(params)
+    if (data.length === 0) {
+        throw new Error("Not found")
+    }
+    return data.slice(0, limit)
+}
+
+
 export {
-    fetchData
+    fetchOne,
+    fetchMany
 }
